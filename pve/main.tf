f9958d57c5673593dac00908c17bcb5fed62d6cb
popvm <willh.harris@gmail.com>
2024-10-16 11:34:55 -0700

    added main and provider. Omitted .tfvars

--- pve/main.tf
+++ pve/main.tf
@@ -1,9 +1,9 @@
 # for cloud-init 
-resource "proxmox_vm_qemu" "test_vm" {
+resource "proxmox_cloud_init_disk" "test_vm" {
   name        = "test-vm"
   target_node = "PVE-SVR-000"                      # Replace with your Proxmox node name
   clone       = "ubuntu-cloud"              # Replace with your VM template name
   cores       = 2
   memory      = 2048
-  #net0        = "virtio,bridge=vmbr0"
+  net0        = "virtio,bridge=vmbr0"
 }
--- pve/provider.tf
+++ pve/provider.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     proxmox = {
@@ -7,10 +8,20 @@ terraform {
   }
 }
 
+variable "pve_username" {
+    description = "proxmox VE username"
+    }
+
+variable "pve_password" {
+    description = "proxmox VE password"
+    type = string
+    sensitive = true 
+    }
+
 provider "proxmox" {
   pm_debug        = true  
   pm_api_url      = "https://192.168.1.20:8006/api2/json"
-  pm_user         = " "  # Replace with your Proxmox user
-  pm_password     = " "        # Replace with your password
+  pm_user         = var.pve_username  # Replace with your Proxmox user
+  pm_password     = var.pve_password        # Replace with your password
   pm_tls_insecure = true                    # Set to true if you are using a self-signed certificate
 }
